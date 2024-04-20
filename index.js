b089721015c024ccd7404cab99fc76452301aa41
AIJCBT <162112108+AIJCBT@users.noreply.github.com>
2024-04-20 15:38:05 +0200

    final editing of package.json to set final dependencies

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer-extra');
 var userAgent = require('user-agents');
 const env = require('dotenv').config()
 const functions = require('./functions')
+const {MongoClient} = require('mongodb');
 
 const email = process.env.email;
 const PW = process.env.PW;
@@ -11,4 +12,12 @@ const stealthplugin = require('puppeteer-extra-plugin-stealth');
 const UserAgent = require('user-agents');
 puppeteer.use(stealthplugin())
 
-browser()
+async function connect(){
+    const url = "mongodb://127.0.0.1:27017/MA"
+    const uri = "mongodb+srv://nodescript:nodescriptpw@mongodb://127.0.0.1:27017/MA?retryWrites=true&w=majority";
+    const client = await new MongoClient(url)
+    await client.connect()
+    await functions.browser(puppeteer, userAgent, email, PW, userAgents, client)
+    await client.close()
+}
+connect(puppeteer, userAgent, email, PW, userAgents)
